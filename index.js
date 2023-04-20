90cdf4104aee2cd2f7fa139eee20e312acd2b180
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-04-20 11:04:38 -0300

    feat: add limiter requests

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const router = require('./src/routers/userRouter')
+const rateLimit = require('express-rate-limit')
 
 app.use(express.json())
 app.use(cors())
+
+const limiter = rateLimit({
+    windowMs: 60 * 1000,
+    max: 8,
+    message: { error: 'Too many requests from this IP, please try again later'}
+})
+
+app.use(limiter)
 app.use(router)
+
+
 
 
 const PORT = process.env.PORT || 3333
