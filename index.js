cc3bc05d09c107f93718af3016ae53d047e8b852
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-04-20 10:44:47 -0300

    feat: update index

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const router = require('./routers/userRouter')
+const router = require('./src/routers/userRouter')
 
 app.use(express.json())
 app.use(cors())
