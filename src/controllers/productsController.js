dcc05b67e6a8ce2325f305c463dd89e18f392fcf
Gabriel Souza <gabriel.souza2004@outlook.com.br>
2023-04-20 10:41:13 -0300

    feat: add new router

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -6,6 +6,15 @@ const getAllProductsController = (request, response) => {
     response.json(products)
 }
 
+const getIdProducts = (request, response) => {
+    const id = Number(request.params.id)
+    const products = getAllProducts()
+    const product = products.find(product => product.id === id)
+
+    product ? response.json(product) : response.status(404).json({ 'error': 'product not found' })
+}
+
 module.exports = {
-    getAllProductsController
+    getAllProductsController, 
+    getIdProducts
 }
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const productsController = require('../controllers/productsController')
 
 router.get('/api/products', productsController.getAllProductsController)
+router.get('/api/products/:id', productsController.getIdProducts)
 
 module.exports = router
